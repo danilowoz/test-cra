ddda8635c9d6a7621ebd88f61452bd26b88dda5f
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-11-16 15:50:47 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-linking"
           target="_blank"
           rel="noopener noreferrer"
         >
