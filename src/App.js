a8e8f448ab9a848e901ddf09295dd720bdc6ba25
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-12-09 14:36:31 +0000

    First Commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './styles.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="NewApp">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
