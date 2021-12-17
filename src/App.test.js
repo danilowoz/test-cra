e7c13247d48f24987e9269c0632344d67cc2b1bf
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-12-17 10:40:14 +0200

    Committed from CodeSandbox

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("render how to learn react link", () => {
+test("render to learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
