66a568e878fafd53b3f660996a20f5b5f344673c
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-12-20 12:36:57 +0200

    local commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> to reload.
+          Edit <code>src/App.js</code> to reload
         </p>
         <a className="App-linking" target="_blank" rel="noopener noreferrer">
           Learn React
