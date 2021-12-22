e74322608dd2e39deeb2508338c1d93f69619315
Alex Moldovan <alex.n.moldovan@gmail.com>
2021-12-22 10:36:36 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn by doing
         </a>
       </header>
     </div>
