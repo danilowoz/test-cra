8c6b84813decdffcca760daba8b3b294aa31608e
Alex Moldovan <alex.n.moldovan@gmail.com>
2022-01-13 10:14:02 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,13 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>  to reload.
+          Edit <code>src/App.js</code> to reload.
         </p>
-        <a
-          className="App-linking"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-linking" target="_blank" rel="noopener noreferrer">
           Learn by doing
         </a>
       </header>
